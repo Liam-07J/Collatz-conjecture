02a91199cd0a9f0db3d5f192eb26aee0805dbdde
Liam-07J <94325296+Liam-07J@users.noreply.github.com>
2022-10-23 18:42:21 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A python file that will check a group of numbers against the Collatz Conjecture and then plot this into a graph
+
+To change the amount of numbers searched change the variable "numberBeingSearched" and set it to the amount of numbers you wish to search. By defult this variable is set to 25
+
+The numbers calculated will be printed out to the terminal
+
+Run the code by typing "python main.py" 
---
+++ main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+plt.title('Collatz Conjecture')
+plt.xlabel("Amount of Iterations until 1 is reached")
+plt.ylabel("Value of the N")
+
+
+def collatz(input):
+    y = []
+    i = 0
+    orginalNumber = input
+    while input != 1:
+        if input % 2 == 0:  # if number is even divide it by 2
+            input = input / 2
+        else:
+            input = (input * 3) + 1 #if number is odd multiply by 3 then + 1
+        y.append(input)  # add the number to a list so that it can be plotted
+        i += 1
+    print(str(orginalNumber) + " fits this pattern")
+    # Make a list x and fill it with intergers from 1 to the size of y
+    x = []
+    for i in range(len(y)): 
+        x.append(i+1)
+    print(x)
+    print(y)
+    plt.plot(x, y, label = orginalNumber)
+
+numberBeingSearched = 25
+i=1
+while i<=numberBeingSearched: 
+    collatz(i)
+    i+=1
+
+print("Data Calculated... Now loading graph")
+
+if numberBeingSearched <= 30:   # This is checked because labels other 30 cannot be displayed
+    plt.legend()
+
+plt.show()
